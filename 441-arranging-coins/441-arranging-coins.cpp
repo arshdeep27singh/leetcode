8bb7b9fcdc0cdf7957fb15135e7fece361b0b44e
Arshdeep Singh <60353069+arshdeep27singh@users.noreply.github.com>
2022-05-20 09:47:52 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (71.87%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        if(n==1)
-            return 1;
-        long long int ctr = 0;
-        for(int i=1; i<=n; i++){
-            ctr += i;
-            if(ctr > n)
-                return i-1;
-            else if(ctr == n)
-                return i;
+        // if(n==1)
+        //     return 1;
+        // long long int ctr = 0;
+        // for(int i=1; i<=n; i++){
+        //     ctr += i;
+        //     if(ctr > n)
+        //         return i-1;
+        //     else if(ctr == n)
+        //         return i;
+        // }
+        // return 0;
+        
+        int left = 0, right = n;
+        long curr, k;
+        while(left <= right){
+            k = left + (right- left)/2;
+            curr = k * (k+1)/2;
+            
+            if(curr == n)
+                return k;
+            else if(curr < n)
+                left = k + 1;
+            else
+                right = k - 1;
         }
-        return 0;
+        return right;
     }
 };
