ebcd84b0ae783d45ee652e7ff21ed884c2bd332d
Arshdeep Singh <60353069+arshdeep27singh@users.noreply.github.com>
2022-08-11 09:01:03 +0530

    Time: 60 ms (45.15%), Space: 71.4 MB (51.04%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -0,0 +1,82 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* reverse(ListNode* head){
+        ListNode* curr = head, *prev = nullptr, *next = nullptr;
+        
+        while(curr != NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+    void insertAtTail(ListNode* &head, ListNode* &tail, int val){
+        
+        ListNode* temp = new ListNode(val);
+        
+        if(head == NULL){
+            head = temp;
+            tail = temp;
+            return;
+        } else {
+            tail->next = temp;
+            tail = temp;
+        }
+    }
+
+    ListNode* add(ListNode* first, ListNode* second){
+        
+        ListNode* ansHead = nullptr, * ansTail = nullptr;
+        int carry = 0;
+        
+        while(first != NULL || second!= NULL || carry != 0){
+            
+            int val1 = 0;
+            if(first != nullptr)
+                val1  = first->val;
+            
+            int val2 = 0;
+            if(second != nullptr)
+                val2 = second->val;
+            
+            int sum = val1 + val2 + carry;
+            int digit = sum % 10;
+            
+            //create node and add in answer linked list
+            insertAtTail(ansHead, ansTail, digit);
+            carry = sum/10;
+            
+            if(first != nullptr) first = first->next;
+            if(second != nullptr ) second = second->next;
+        }
+        
+        return ansHead;
+    }
+   
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        // // 1 - reverse both the lists
+        // l1 = reverse(l1);
+        // l2 = reverse(l2);
+        
+        // 2 -  add the reverse lists
+        ListNode* ans = add(l1, l2);
+        
+        // 3 - reverse the added lists
+        // ans = reverse(ans);
+        return ans;
+    }
+};
